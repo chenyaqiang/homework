c63c1bee7e15e79028b03d44b992277697d1f642
xyc <460206886@qq.com>
2018-03-10 17:30:59 +0800

    +sigleton+prototype

--- sigletonandprototype/sigleton/regist/PhoneFactory.java
+++ sigletonandprototype/sigleton/regist/PhoneFactory.java
@@ -6,17 +6,18 @@ import java.util.concurrent.ConcurrentHashMap;
 //每使用一个对象前都先都将其注册到一个（线程安全的）固定（保证存取对象一致性）的容器中，
 // 从容器中获取实例使用，但是注册（存）的过程会有线程安全问题使创建不唯一
 public class PhoneFactory {
-    private ConcurrentHashMap<String, PhoneRegist> collection = new ConcurrentHashMap();
+    private  ConcurrentHashMap<String, Object> collection = new ConcurrentHashMap();
 
-    public PhoneRegist getPhone(String name) {
+    public  Object getPhone(String name) {
         if (collection.containsKey(name)) {
             return collection.get(name);
         } else {
-            collection.put(name, new PhoneRegist());//注册登记过程
+            try {
+                collection.put(name,Class.forName(name).getInterfaces());//注册登记过程
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             return collection.get(name);
         }
     }
-}
-
-class PhoneRegist {
 }
